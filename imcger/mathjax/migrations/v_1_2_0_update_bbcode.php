20895b559f0dae7dc62d6993a2e1fdfe8b8a8629
Thorsten <88725484+IMC-GER@users.noreply.github.com>
2025-03-30 15:06:57 +0200

    v1.2.0
    Improved compatibility with the previous extension
    - Changed BBCode `[latex]` to inline
    - Added BBCode `[latex_para]` to insert as paragraph

---
+++ imcger/mathjax/migrations/v_1_2_0_update_bbcode.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * MathJax
+ * An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2025, Thorsten Ahlers
+ * @copyright (c) 2018, kinerity, https://www.layer-3.org
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace imcger\mathjax\migrations;
+
+class v_1_2_0_update_bbcode extends \phpbb\db\migration\migration
+{
+	protected static $bbcodes_installer;
+
+	protected $bbcode_data = [
+			'LaTeX' => [
+				'bbcode_helpline'	 => 'Shows the LaTeX formula inline.',
+				'bbcode_match'		 => '[latex #ignoreTags=true]{TEXT}[/latex]',
+				'bbcode_tpl'		 => '<span>\({TEXT}\)</span>',
+				'display_on_posting' => 1,
+			],
+			'LaTeXinline' => [
+				'bbcode_helpline'	 => 'Shows the LaTeX formula inline.',
+				'bbcode_match'		 => '[latex_inline #ignoreTags=true]{TEXT}[/latex_inline]',
+				'bbcode_tpl'		 => '<span>\({TEXT}\)</span>',
+				'display_on_posting' => 0,
+			],
+			'LaTeXparagraph' => [
+				'bbcode_helpline'	 => 'Inserting the LaTeX formula centered in a separate paragraph.',
+				'bbcode_match'		 => '[latex_para #ignoreTags=true]{TEXT}[/latex_para]',
+				'bbcode_tpl'		 => '<span>\[{TEXT}\]</span>',
+				'display_on_posting' => 1,
+			],
+		];
+
+	public static function depends_on()
+	{
+		return ['\imcger\mathjax\migrations\v_1_0_0_add_bbcode',];
+	}
+
+	public function update_data()
+	{
+		return [
+			['custom', [[$this, 'update_bbcodes_table']]],
+		];
+	}
+
+	public function revert_data()
+	{
+		return [
+			['custom', [[$this, 'delete_bbcodes']]],
+		];
+	}
+
+	public function update_bbcodes_table()
+	{
+		if (!isset($this->bbcodes_installer))
+		{
+			$this->bbcodes_installer = new \imcger\mathjax\core\bbcodes_installer($this->db, $this->phpbb_root_path, $this->php_ext);
+		}
+
+		$this->bbcodes_installer->install_bbcodes($this->bbcode_data);
+	}
+
+	public function delete_bbcodes()
+	{
+		if (!isset($this->bbcodes_installer))
+		{
+			$this->bbcodes_installer = new \imcger\mathjax\core\bbcodes_installer($this->db, $this->phpbb_root_path, $this->php_ext);
+		}
+
+		$this->bbcodes_installer->delete_bbcodes($this->bbcode_data);
+	}
+}
